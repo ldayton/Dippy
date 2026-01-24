1f26808e1e8f86c19695336a14f5a6d80d7b20b6
Lily Dayton <lily.dayton.git@gmail.com>
2026-01-24 14:22:46 +0200

    Update vendored parable.py to 668cee26
    Brings in refactored bracket matching with _skip_matched_pair() and
    _skip_subscript() mirroring bash's subst.c delegation pattern.

--- src/dippy/vendor/__init__.py
+++ src/dippy/vendor/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 # Vendored dependencies
 #
-# parable.py: https://github.com/ldayton/Parable @ 8fb0a4d5a475e001bfc4cd90a1244c117ec25962
+# parable.py: https://github.com/ldayton/Parable @ 668cee26a0f2dde024e2ec7f5da7733a28e0ba0d
